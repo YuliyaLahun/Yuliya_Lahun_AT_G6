41288d2e9470372f9d7818ed0559656e419f3872
YuliyaLahun <yuliyalagun@gmail.com>
2022-10-25 00:17:02 +0200

    tasks 0 -3 from hometask day8

--- src/main/java/homework/day2/basetask/TrainMethodsObjects.java
+++ src/main/java/homework/day2/basetask/TrainMethodsObjects.java
@@ -2,31 +2,31 @@ package basetask;
 
 public class TrainMethodsObjects {
 
-    public void processMouse(Mouse mouse) {
+    public void processMouse(basetask.Mouse mouse) {
         System.out.println(mouse.getName());
         System.out.println(mouse.getAge());
         mouse.printMouseDetails();
     }
 
-    public void processSouce(Souce souce) {
+    public void processSouce(basetask.Souce souce) {
         System.out.println(souce.getColor());
         System.out.println(souce.getName());
         souce.printSouceDetails();
     }
 
-    public void processBee(Bee bee) {
+    public void processBee(basetask.Bee bee) {
         System.out.println(bee.getGender());
         System.out.println(bee.getWeight());
         bee.printBeeDetails();
     }
 
-    public void processObstacle(Obstacle obstacle) {
+    public void processObstacle(basetask.Obstacle obstacle) {
         System.out.println(obstacle.getDescription());
         System.out.println(obstacle.getSeverity());
         obstacle.printObstacleDetails();
     }
 
-    public void processPineapple(Pineapple pineapple) {
+    public void processPineapple(basetask.Pineapple pineapple) {
         System.out.println(pineapple.getGrade());
         System.out.println(pineapple.getHeatCapacity());
         pineapple.printPineappleDetails();
