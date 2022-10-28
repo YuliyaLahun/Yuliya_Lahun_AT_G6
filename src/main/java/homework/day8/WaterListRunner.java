850563e8dfacfa00d3c7e90d4b7b9159a4addd9d
YuliyaLahun <yuliyalagun@gmail.com>
2022-10-28 10:11:40 +0200

    finished water

---
+++ src/main/java/homework/day8/WaterListRunner.java
@@ -0,0 +1,19 @@
+package homework.day8;
+
+import homework.day8.classes.Water;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WaterListRunner {
+
+    public static void main(String[] args) {
+        List<Water> waterList = Arrays.asList(new Water("Прозрачная", "Нет"),
+                new Water("Прозрачная","Нет"),
+                new Water("Мутная","Аммиачный"));
+
+        for (Water water: waterList){
+            System.out.println(water.getColor()+"-"+water.getSmell());
+        }
+    }
+}
