de0843ef47778aa3b58df91275a76be0b103f224
YuliyaLahun <yuliyalagun@gmail.com>
2022-11-02 18:25:13 +0100

    GenerateListOfInts task updated

--- src/main/java/homework/day10/GenerateListOfInt.java
+++ src/main/java/homework/day10/GenerateListOfInt.java
@@ -2,6 +2,8 @@ package homework.day10;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 //сгенерировать коллекцию целых чисел (List) из n элементов
 // от minValue до maxValue и определить,
@@ -9,12 +11,12 @@ import java.util.List;
 public class GenerateListOfInt {
 
     public static void main(String[] args) {
+
         int minValue = 1;
         int maxValue = 15;
-        List<Integer> list = new ArrayList<>();
-        for (int i = minValue; i <= maxValue; i++) {
-            list.add(i);
-        }
+
+        List<Integer> list = IntStream.rangeClosed(minValue, maxValue).boxed().collect(Collectors.toList());
+
         System.out.println("Содержатся числа, которые делятся и на 3 и на 5 без остатка: "
                 + list.stream().anyMatch(x -> x % 3 == 0 && x % 5 == 0));
 
