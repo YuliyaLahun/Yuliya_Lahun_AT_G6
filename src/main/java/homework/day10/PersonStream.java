75f3806208ed31698a510bbd730b21c4893c7c79
YuliyaLahun <yuliyalagun@gmail.com>
2022-11-03 20:10:12 +0100

    Person task updated

--- src/main/java/homework/day10/PersonStream.java
+++ src/main/java/homework/day10/PersonStream.java
@@ -5,19 +5,19 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PersonStream {
 
     public static void main(String[] args) {
 
-        List<Person> people = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            people.add(new Person(getRandomNumberUsingNextInt(15, 31), "name" + i, "surname" + i));
-        }
+        List<Person> people = IntStream.rangeClosed(1, 100).boxed()
+                .map(i -> new Person(getRandomNumberUsingNextInt(15, 31),"name " + i, "surname" + i))
+                .collect(Collectors.toList());
 
         System.out.println(people.stream()
                 .filter(x -> x.getAge() > 21)
-                .peek(x -> System.out.println("name" + x.getName() + " age:" + x.getAge()))
+                .peek(x -> System.out.println("name: " + x.getName() + " age:" + x.getAge()))
                 .sorted(Comparator.comparing(Person::getSurname).thenComparing(Person::getName))
                 .limit(4)
                 .map(Person::getName)
